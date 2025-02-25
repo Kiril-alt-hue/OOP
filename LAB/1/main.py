5e65e3da3229d46ef008877a15ac8e8c9a968a41
Kiril-alt-hue <kirabatrak@gmail.com>
2025-02-25 13:57:42 +0200

    Доробили)))

--- LAB/1/main.py
+++ LAB/1/main.py
@@ -1,10 +1,11 @@
 from t01 import QuadraticEquation
 
-equations = [] # буде містити список всіх рівнянь прочитаних з файлу
-none_solut = [] # немає розв'язків
-one_solut = [] # один розв'язок
-two_solut = [] # два розв'язки
-inf_solut = [] # нескінченна кількість розв'язків
+equations = []  # буде містити список всіх рівнянь прочитаних з файлу
+none_solut = []  # немає розв'язків
+one_solut = []  # один розв'язок
+two_solut = []  # два розв'язки
+inf_solut = []  # нескінченна кількість розв'язків
+
 with open("input01.txt") as f:
     for line in f:
         print(line)
@@ -14,31 +15,28 @@ with open("input01.txt") as f:
         except ValueError:
             pass
 
+maxim_solut = [None, None]
+minim_solut = [None, None]
+
 for eq in equations:
-    maxim_solut = [None, None]
-    minim_solut = [None, None]
     solution = eq.solve()
-    #if len(solution) == 1 and solution[0] == None:
+
     if solution == QuadraticEquation.INF:
         inf_solut.append(eq)
     elif len(solution) == 0:
         none_solut.append(eq)
     elif len(solution) == 1:
         one_solut.append(eq)
-        if maxim_solut[0] is None:
+        if maxim_solut[0] is None or solution[0] > maxim_solut[0]:
             maxim_solut = [solution[0], eq]
+        if minim_solut[0] is None or solution[0] < minim_solut[0]:
             minim_solut = [solution[0], eq]
-        else:
-            if solution[0] > maxim_solut[0]:
-                maxim_solut = [solution[0], eq]
-            if solution[0] < minim_solut[0]:
-                minim_solut = [solution[0], eq]
-
     elif len(solution) == 2:
         two_solut.append(eq)
 
-
-print(f"Рівняння, що мають 0 розв'язків - {}")
-
-
-
+print(f"Рівняння, що мають 0 розв'язків - {none_solut}")
+print(f"Рівняння, що мають 1 розв'язок - {one_solut}")
+print(f"Найбільший розв'язок для рівнянь, що мають 1 корінь - {maxim_solut}")
+print(f"Найменший розв'язок для рівнянь, що мають 1 корінь - {minim_solut}")
+print(f"Рівняння, що мають 2 розв'язки - {two_solut}")
+print(f"Рівняння, що мають безліч розв'язків - {inf_solut}")
--- LAB/1/t01.py
+++ LAB/1/t01.py
@@ -1,3 +1,5 @@
+import copy
+
 class QuadraticEquation:
 
     INF = "Infinite number of solutions"
@@ -7,58 +9,55 @@ class QuadraticEquation:
         self.b = b
         self.c = c
 
-
-        # self.d = self.b ** 2 - 4 * self.a * self.c # дискримінант - не можна,
-        # # бо може порушитися цілісність даних, отже робимо метод
-
     def __str__(self):
         return f"{self.a}x^2 + {self.b}x + {self.c} = 0"
 
     def discriminant(self):
         return self.b ** 2 - 4 * self.a * self.c
 
-    #def show(self):
-        #print(self)
-
-    #def solutions_number(self):
-        # залежить від дискримінанту
-        #return 0
-
     def solve(self):
         solve_lst = []
-        if self.a == 0: # вироджене квадратне рівняння
+        if self.a == 0:  # вироджене квадратне рівняння
             if self.b == 0:
                 if self.c == 0:
                     return QuadraticEquation.INF
                 else:
-                    return () #"Розв'язків не існує"
+                    return ()  # "Розв'язків не існує"
             else:
-                  solve_lst.append(-self.c/self.b)
-                  return solve_lst                                  #return -self.c / self.b
+                solve_lst.append(-self.c / self.b)
+                return solve_lst  # return -self.c / self.b
         else:
             d = self.discriminant()
-            if d < 0: # немає розв'язків
-                return ()# "Дискримінант менше за нуль -> Розв'язків не існує"
-            elif d == 0: # є один розв'язок
-                solve_lst.append((-self.b)/ (2 * self.a))
-            else: # два корені
-                solve_lst.append(-self.b + d ** 0.5/ (2 * self.a))
-                solve_lst.append((-self.b - d ** 0.5)/ (2 * self.a))
+            if d < 0:  # немає розв'язків
+                return ()  # "Дискримінант менше за нуль -> Розв'язків не існує"
+            elif d == 0:  # є один розв'язок
+                solve_lst.append((-self.b) / (2 * self.a))
+            else:  # два корені
+                solve_lst.append((-self.b + d ** 0.5) / (2 * self.a))
+                solve_lst.append((-self.b - d ** 0.5) / (2 * self.a))
 
-            return sorted(solve_lst) # список чи кортеж розвʼязків
+            return sorted(solve_lst)  # список чи кортеж розвʼязків
 
+    def __copy__(self):
+        # Створюємо новий об'єкт з тими самими значеннями a, b, c
+        return QuadraticEquation(self.a, self.b, self.c)
+
+    #def __deepcopy__(self, memo):
+        # Для глибокого копіювання (у цьому випадку не потрібно, оскільки атрибути прості)
+        #return QuadraticEquation(copy.deepcopy(self.a, memo),
+                                 #copy.deepcopy(self.b, memo),
+                                 #copy.deepcopy(self.c, memo))
 
 
 if __name__ == '__main__':  # блок тестування класу
-    eq = QuadraticEquation(0, 0, 0 )
-    # eq.show()
-
+    eq = QuadraticEquation(1, -3, 2)
     print(eq)
     print(f"Дискримінант рівняння {eq} буде {eq.discriminant()}."
           f" Розв'язки рівняння -> {eq.solve()}")
-          #f"Розв'язки рівняння - {}")
-    # eq.discriminant() = 100500
-    #eq.a = 100560
-    #print(f"дискримінант рівняння {eq} буде {eq.discriminant()}")
-    # r = str(eq)
-    # print(r)
+
+    # Тестування конструктора копіювання
+    eq_copy = copy.copy(eq)
+    print("\nКопія рівняння:")
+    print(eq_copy)
+    print(f"Дискримінант копії рівняння {eq_copy} буде {eq_copy.discriminant()}."
+          f" Розв'язки копії рівняння -> {eq_copy.solve()}")
